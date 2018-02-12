18d592cc1a5cde00d5022e5e305c446ebe69dc76
Anderson Pereira <limaand@gmail.com>
2018-02-12 00:59:16 -0300

    modulo validador express para formulário

--- app/routes/admin.js
+++ app/routes/admin.js
@@ -6,6 +6,21 @@ module.exports = function(app){
   app.post('/noticias/salvar', function(req,res){
    var noticiasJson = req.body;
    
+   req.assert('titulo', 'Titulo é obrigatorio').notEmpty(); 
+   req.assert('noticia', 'Notícia é obrigatorio').notEmpty(); 
+   req.assert('noticia', 'Conteúdo deve conter entre 10 e 100 caracteres').len(10,100); 
+   req.assert('autor', 'Autor é obrigatorio').notEmpty();
+   req.assert('data_noticia', 'Data é obrigatorio').notEmpty();
+    
+
+  
+  var erros = req.validationErrors(); 
+
+  //console.log(erros);
+  if(erros){
+   res.render("admin/form_add_noticia", {validacao: erros});
+   return;
+  }                       
 
 
    //salvarNoticia
--- config/server.js
+++ config/server.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var consign = require("consign");
 var bodyParser = require("body-parser");
+var expressValidator = require("express-validator");
 
 var app = express();
 
@@ -9,7 +10,7 @@ app.set('views', './app/views');
 
 /*midleware*/
 app.use(bodyParser.urlencoded({extended:true}));
-
+app.use(expressValidator());
 /* todas as rotas*/
 consign()
   .include('app/routes').into(app)
